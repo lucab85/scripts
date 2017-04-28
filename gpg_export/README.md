8832b47bf153798500e04a4c7619f7c7013021a2
Luca Berton <mr.evolution85@gmail.com>
2017-04-28 14:54:05 +0200

    gpg export

---
+++ gpg_export/README.md
@@ -0,0 +1,6 @@
+# Gpg export
+
+A simple bash script to export public, private and fingerprint of a GPG identity.
+Usage: ./gpg_export e-mail/gpg-id
+
+Require a working gpg with a key already generated.
---
+++ gpg_export/gpg_export.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]
+then
+	echo "Usage: $0 e-mail/gpg-id"
+	exit 1
+fi
+gpg --export --armor $1 > $1_public.asc
+gpg --export-secret-key --armor $1 > $1_private.key
+gpg --fingerprint > $1_fingerprint.txt
