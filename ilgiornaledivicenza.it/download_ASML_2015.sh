164bb9cff16d1cb6d247274688fd6ca3820a668d
Luca Berton <mr.evolution85@gmail.com>
2017-06-07 09:06:00 +0200

    Parametrize variable into script

--- ilgiornaledivicenza.it/download_ASML_2015.sh
+++ ilgiornaledivicenza.it/download_ASML_2015.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]
+then
+	echo "Usage: $0 token counter"
+	exit 1
+fi
+
 
 COUNTER=1
-while [  $COUNTER -lt 150 ]; do
+while [  $COUNTER -lt $2 ]; do
 	echo Salvo pagina $COUNTER
 
 	URL="http://sfogliatoreweb.ilgiornaledivicenza.it/vpweb/magazines/ASVI/issues/ASML_2015_2015/pdfs/$COUNTER"
 	wget \
 	 --header="User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36" \
 	 --header="Accept-Encoding: gzip, deflate, sdch" \
-	 --header="Cookie:thirdparty.check=735; ticket=009d8e93812e5bd81c29679a2d6e52a4bdc96b89" \
+	 --header="Cookie:thirdparty.check=735; ticket=$1" \
 	 $URL \
 	-O "$COUNTER.pdf" 
 	#curl -O $URL
