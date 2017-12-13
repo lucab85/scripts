ea344d6d0c533a77fa0671ad8f0a638d80fe637f
Luca Berton <mr.evolution85@gmail.com>
2017-12-13 11:26:16 +0100

    Add image_noresize to Gulp image optimization pipeline

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -6,7 +6,8 @@ const imageResize = require('gulp-image-resize')
 const imagemin = require('gulp-imagemin');
 const jpegrecompress = require('imagemin-jpeg-recompress');
 
-var IMG_SRC = 'images/*.{jpg,png,gif}';
+var IMG_SRC = 'images/*.{jpg,png,gif,svg}';
+var IMG_NORES_SRC = 'images_noresize/*.{jpg,png,gif}';
 var IMG_DST = '../static/images/';
 var IMG_W = 600;
 
@@ -24,6 +25,14 @@ var opts_jpegrecompress = {
 }
 var plugins_imagemin = [
 	jpegrecompress(opts_jpegrecompress),
+	imagemin.optipng({optimizationLevel: 5}),
+	imagemin.gifsicle({interlaced: true}),
+		imagemin.svgo({
+		plugins: [
+			{removeViewBox: true},
+			{cleanupIDs: false}
+		]
+	})
 ]
 var opts_imagemin = {
 	verbose: true
@@ -41,6 +50,13 @@ gulp.task('images', function () {
         .pipe(gulp.dest(IMG_DST))
 });
 
+gulp.task('images_noresize', function () {
+    return gulp.src(IMG_NORES_SRC)
+		.pipe(newer(IMG_DST))
+        .pipe(imagemin(plugins_imagemin, opts_imagemin))
+        .pipe(gulp.dest(IMG_DST))
+});
+
 gulp.task('favicons', function () {
     return gulp.src(FAV_SRC)
 		.pipe(newer(FAV_DST))
@@ -48,4 +64,4 @@ gulp.task('favicons', function () {
         .pipe(gulp.dest(FAV_DST))
 });
 
-gulp.task('default', ['images', 'favicons']);
+gulp.task('default', ['images', 'images_noresize', 'favicons']);
